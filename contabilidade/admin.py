d661fa272e91da60387714f0dfe8888b863942fe
Fábio Moraes <fmoraes05@outlook.com>
2022-12-21 19:43:48 -0300

    Projeto contábil registrando os models

--- contabilidade/admin.py
+++ contabilidade/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Cargo, Equipe, Conta, Nota, Razao
 
-# Register your models here.
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'criado', 'modificado', 'ativo')
+
+@admin.register(Equipe)
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'dataNascimento', 'email', 'cargo', 'criado', 'modificado', 'ativo')
+
+@admin.register(Conta)
+class ContaAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'nomConta', 'slug', 'criado', 'modificado', 'ativo')
+
+@admin.register(Nota)
+class NotaAdmin(admin.ModelAdmin):
+    list_display = ('dataNota', 'contaCredito', 'contaDebito', 'valor', 'imprimir', 'criado', 'modificado', 'ativo')
+
+@admin.register(Razao)
+class RazaoAdmin(admin.ModelAdmin):
+    list_display = ('nomeConta', 'periodoInicial', 'periodoFinal', 'saldoAnterior', 'entradas', 'saidas', 'situacao', \
+        'criado', 'modificado', 'ativo')
+        
--- contabilidade/models.py
+++ contabilidade/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.db.models import signals
 from django.template.defaultfilters import slugify
 from stdimage.models import StdImageField
+import uuid
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
 
 class Base(models.Model):
     criado = models.DateField('Data de Criação', auto_now_add=True)
@@ -27,7 +33,7 @@ class Equipe(Base):
     dataNascimento = models.DateField('Data de Nascimento')
     email = models.EmailField('E-mail', max_length=100)
     cargo = models.ForeignKey('contabilidade.Cargo', verbose_name='Cargo', on_delete=models.CASCADE, default=0)
-    foto = StdImageField('Foto', upload_to='equipe',variations={'thumb': {'width': 480, 'height':480, 'crop':True}})
+    foto = StdImageField('Foto', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height':480, 'crop':True}})
 
     class Meta:
         verbose_name = 'Equipe'
@@ -60,8 +66,8 @@ signals.pre_save.connect(conta_pre_save, sender=Conta)
 
 class Nota(Base):
     dataNota = models.DateField('Data')
-    contaCredito = models.ForeignKey('contabilidade.Conta', related_name='Credito', verbose_name='Conta', on_delete=models.CASCADE, default=0)
-    contaDebito = models.ForeignKey('contabilidade.Conta', related_name='Debito', verbose_name='Conta', on_delete=models.CASCADE, default=0)
+    contaCredito = models.ForeignKey('contabilidade.Conta', related_name='Credito', verbose_name='Conta Crédito', on_delete=models.CASCADE, default=0)
+    contaDebito = models.ForeignKey('contabilidade.Conta', related_name='Debito', verbose_name='Conta Débito', on_delete=models.CASCADE, default=0)
     valor = models.DecimalField('Valor', max_digits=9, decimal_places=2)
     imprimir = models.BooleanField('Imprimir?', default=True)
 
@@ -81,3 +87,10 @@ class Razao(Base):
     saidas = models.DecimalField('Saídas', max_digits=9, decimal_places=2)
     saldoFinal = models.DecimalField('Saldo Final', max_digits=9, decimal_places=2)
     situacao = models.BooleanField('Situação?', default=True)
+
+    class Meta:
+        verbose_name = 'Razao'
+        verbose_name_plural = 'Razoes'
+
+    def __str__(self):
+        return self.nomeConta
