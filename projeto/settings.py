301ffd08f6d85a095b766ffa4f337baa16e6bf4a
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2026-06-03 19:49:45 -0300

    Ajustado o settings

--- projeto/settings.py
+++ projeto/settings.py
@@ -119,4 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIRS = [BASE_DIR / 'financeiro' / 'static']
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'financeiro', 'static'),
+    ]
